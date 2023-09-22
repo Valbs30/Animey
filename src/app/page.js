f094909036ddc3d0a3eb3b58610a21164c1625d0
Valbs30 <lucasvalbusa30@gmail.com>
2023-09-21 23:39:16 -0300

    version_1.22

--- src/app/page.js
+++ src/app/page.js
@@ -1,47 +1,30 @@
 import CardAnime from '@/components/CardAnime'
 import Titulo from '@/components/Titulo'
 
-export default function Home() {
+async function carregarDados(){
+	const url = "https://api.jikan.moe/v4/top/anime"
+	const response = await fetch(url)
+	const json = await response.json()
+	return json.results || []
+  }
 
-	const animes = [
-		{
-			id: 1,
-			nome: "Jujutsu Kaisen",
-			nota: 9.5,
-			temp: 2,
-			class: "14",
-			capa: "https://cdn.myanimelist.net/images/anime/1171/109222l.jpg"
-		},
-		{
-			id: 2,
-			nome: "Mushoku Tensei",
-			nota: 9.0,
-			temp: 2,
-			class: "L",
-			capa: "https://cdn.myanimelist.net/images/anime/1530/117776l.jpg"
-		},
-		{
-			id: 3,
-			nome: "Blue Lock",
-			nota: 9.2,
-			temp: 1,
-			class: "L",
-			capa: "https://cdn.myanimelist.net/images/anime/1258/126929l.jpg"
-		}
-	]
-
+export default async function Home() {
+	const animes = await carregarDados()
+	console.log(animes)
 	return ( //JSX
 		<>
-			<nav className="bg-neutral-800 p-4 flex gap-20 items-end">
-				<h1 className="text-3xl font-bold text-zinc-100">Animey</h1>
-				<ul>
-					<li className='flex gap-20'>
-						<a href="#">Mais Vistos</a>
-						<a href="#">Lançamentos</a>
-						<a href="#">Meus Favoritos</a>
-						<a href="#">Sobre</a>
-					</li>
-				</ul>
+			<nav className="bg-neutral-800 px-10 py-4 flex items-end justify-between">
+				<div className="flex items-end gap-20">
+					<h1 className="text-3xl font-bold text-zinc-100">Animey</h1>
+					<ul>
+						<li className="flex gap-20 text-lg">
+							<a href="#">Mais Vistos</a>
+							<a href="#">Lançamentos</a>
+							<a href="#">Meus Favoritos</a>
+						</li>
+					</ul>
+				</div>
+				<a href="#" className="text-lg">Sobre</a>
 			</nav>
 
 			<Titulo>Mais Vistos</Titulo>
--- src/components/CardAnime.jsx
+++ src/components/CardAnime.jsx
@@ -10,7 +10,7 @@ export default function CardAnime({ anime }) {
 
     useEffect(() => {
         let favoritos = JSON.parse(localStorage.getItem("favoritos")) || []
-        const favorito = favoritos.find(a => a.id == anime.id)
+        const favorito = favoritos.find(a => a.id == anime.mal_id)
         setFavorito(favorito)
     }, [])
 
@@ -24,17 +24,17 @@ export default function CardAnime({ anime }) {
     function desfavoritar() {
         setFavorito(false)
         let favoritos = JSON.parse(localStorage.getItem("favoritos")) || []
-        const favoritosAtualizados = favoritos.filter(a => a.id !== anime.id)
+        const favoritosAtualizados = favoritos.filter(a => a.id !== anime.mal_id)
         localStorage.setItem("favoritos", JSON.stringify(favoritosAtualizados))
     }
 
     return (
         <div className="flex flex-col items-center w-40 m-2 gap-1 bg-zinc-700 rounded">
             <div className="relative">
-                <img className="p-1" src={anime.capa} alt="" />
+                <img className="p-1" src={anime.jpg.image_url} alt="" />
 
                 <div className="absolute top-0 left-0 m-1.5 w-9 h-9 rounded border border-black">
-                    <Classificacao classe = {anime.class}></Classificacao>
+                    <Classificacao classe = {anime.rating}></Classificacao>
                 </div>
 
                 {favorito ?
@@ -53,21 +53,21 @@ export default function CardAnime({ anime }) {
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4 text-amber-400">
                         <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z" clip-rule="evenodd" />
                     </svg>
-                    <span className="#">{anime.nota.toFixed(1)}</span>
+                    <span className="#">{anime.score.toFixed(1)}</span>
                 </div>
 
                 <span className="bg-zinc-700 p-1 rounded absolute bottom-0 left-0 m-1.5 text-xs">
-                    {anime.temp === 1 ?
-                        anime.temp + " Temporada"
+                    {anime.episodes === 1 ?
+                        anime.episodes + " Temporada"
                         :
-                        anime.temp + " Temporadas"
+                        anime.episodes + " Temporadas"
                     }
                 </span>
 
             </div>
 
             <span className="text-lg w-full line-clamp-1 text-center">
-                {anime.nome}
+                {anime.title}
             </span>
 
         </div>
